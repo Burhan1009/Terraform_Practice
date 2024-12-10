a28dfd5630c27c954e87c292e892fd6caade89ff
Burhan <techdeployers.com@gmail.com>
2024-12-10 14:10:59 +0530

    Update output.tf

--- TerraformProject/output.tf
+++ TerraformProject/output.tf
@@ -1,8 +1,12 @@
-output "ec2_public_ip" {
-  value = aws_instance.my_ec2[*].public_ip
-}
+# output "ec2_public_ip" {
+#   value = aws_instance.my_ec2[*].public_ip
+# }
 
-# Output the private IP
-output "ec2_private_ip" {
-  value = aws_instance.my_ec2[*].private_ip
+# # Output the private IP
+# output "ec2_private_ip" {
+#   value = aws_instance.my_ec2[*].private_ip
+# }
+# For ke through data fetch karna hoga 
+output "ec2_public_ip" {
+  value = { for k, v in aws_instance.my_ec2 : k => v.public_ip }
 }
